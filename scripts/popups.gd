1647903852d24b2ca004376b7f472f07ecbc6cd1
Barti <wrona1692@gmail.com>
2025-07-23 00:09:50 +0200

    skończony popUp

--- scripts/popups.gd
+++ scripts/popups.gd
@@ -1,12 +1,17 @@
 extends Control
 
+var follow_mouse := false
+
 func ItemPopup(item : Item):
 	if item != null:
 		set_value(item)
-	%ItemPopup.popup()
+		%ItemPopup.popup()
+		follow_mouse = true
+		update_popup_position()
 
 func HideItemPopup():
 	%ItemPopup.hide()
+	follow_mouse = false
 
 func set_value(item : Item):
 	%Name.text = item.name + " +" + str(item.upgrade_lvl)
@@ -14,3 +19,12 @@ func set_value(item : Item):
 	%baseStats.text = item.base_stats
 	%rolls.text = item.rolls
 	%class.text = "[ Do Ubrania ]\n" + item.classes
+
+func _process(_delta):
+	if follow_mouse:
+		update_popup_position()
+
+func update_popup_position():
+	var mouse_pos = get_viewport().get_mouse_position()
+	var offset = Vector2(20, 20)
+	%ItemPopup.position = mouse_pos + offset
