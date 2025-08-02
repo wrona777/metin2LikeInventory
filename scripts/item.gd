113ba15a6c4702363836fe5d8915ad631fc29a0a
Barti <wrona1692@gmail.com>
2025-08-02 18:20:00 +0200

    JEST PROGRESS!

--- scripts/item.gd
+++ scripts/item.gd
@@ -13,9 +13,9 @@ func _ready() -> void:
 	if item:
 		itemSetter()
 
-func _process(_delta: float) -> void:
-	if dragged_item_preview:
-		_update_dragged_position()
+#func _process(_delta: float) -> void:
+	#if dragged_item_preview:
+		#_update_dragged_position()
 
 func itemSetter() -> void:
 	texture.custom_minimum_size = Vector2(item.width,item.height)
@@ -29,32 +29,37 @@ func _on_texture_mouse_entered() -> void:
 func _on_texture_mouse_exited() -> void:
 	Popups.HideItemPopup()
 
-func _on_texture_gui_input(event: InputEvent) -> void:
-	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-		original_parent = itemNode.get_parent() #Slot
-		createPreview()
-		saved_item = item.duplicate()
-		texture.hide()
+#func _on_texture_gui_input(event: InputEvent) -> void:
+	#if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+		#original_parent = itemNode.get_parent() #Slot
+		#createPreview()
+		#saved_item = item.duplicate()
+		#texture.hide()
+#
+#func createPreview():
+	#dragged_item_preview = TextureRect.new()
+	#dragged_item_preview.texture = texture.texture
+	#dragged_item_preview.mouse_filter = Control.MOUSE_FILTER_IGNORE
+	#
+	#dragged_item_preview.stretch_mode = TextureRect.STRETCH_SCALE
+	#dragged_item_preview.ignore_texture_size = true
+	#dragged_item_preview.size = Vector2(94, 94)
+	#
+	#add_child(dragged_item_preview)
+	#dragged_item_preview.z_index = 999
+	#dragged_item_preview.position = get_local_mouse_position()
+	#set_listening(true)
+#
+#func _update_dragged_position() -> void:
+	#if dragged_item_preview:
+		#var mouse_pos = get_local_mouse_position()
+		#var texture_size = dragged_item_preview.size
+		#dragged_item_preview.position = mouse_pos - texture_size / 2
+#
+#func set_listening(state: bool) -> void:
+	#is_item_dragged = state
 
-func createPreview():
-	dragged_item_preview = TextureRect.new()
-	dragged_item_preview.texture = texture.texture
-	dragged_item_preview.mouse_filter = Control.MOUSE_FILTER_IGNORE
-	
-	dragged_item_preview.stretch_mode = TextureRect.STRETCH_SCALE
-	dragged_item_preview.ignore_texture_size = true
-	dragged_item_preview.size = Vector2(94, 94)
-	
-	add_child(dragged_item_preview)
-	dragged_item_preview.z_index = 999
-	dragged_item_preview.position = get_local_mouse_position()
-	set_listening(true)
-
-func _update_dragged_position() -> void:
-	if dragged_item_preview:
-		var mouse_pos = get_local_mouse_position()
-		var texture_size = dragged_item_preview.size
-		dragged_item_preview.position = mouse_pos - texture_size / 2
-
-func set_listening(state: bool) -> void:
-	is_item_dragged = state
+func _get_drag_data(at_position: Vector2) -> Variant:
+	saved_item = item.duplicate()
+	print(saved_item.name)
+	return saved_item
--- scripts/slot.gd
+++ scripts/slot.gd
@@ -1,1 +1,16 @@
 extends PanelContainer
+
+@onready var panel = $"."
+
+##func _get_drag_data(at_position: Vector2) -> Variant:
+	##print("essa!")
+	##return true
+
+func _can_drop_data(at_position: Vector2, data: Variant) -> bool:
+	return data is Item
+
+func _drop_data(at_position: Vector2, data: Variant) -> void:
+	var item_scene = preload("res://scenes/item.tscn")
+	var new_item = item_scene.instantiate()
+	new_item.item = data
+	panel.add_child(new_item)
